d1dabd81619903b296366352a1dd9bd5db187b71
Alex Romo III <alexromo3rd@gmail.com>
2017-09-10 15:48:49 -0700

    Working on user authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :wikis, dependent: :destroy
+  attr_accessible :role
+
+  USER_ROLES = {
+    :standard => 0,
+    :premium => 10,
+    :admin => 20
+  }
+
+  def set_as_standard
+    self.role = USER_ROLES[:standard]
+  end
+
+  def set_as_premium
+    self.role = USER_ROLES[:premium]
+  end
+
+  def set_as_admin
+    self.role = USER_ROLES[:admin]
+  end
 end
