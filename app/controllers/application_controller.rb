ef936e5c82b2e8be173529d60e6f5228e2b64ba0
Alex Romo III <alexromo3rd@gmail.com>
2017-09-27 17:46:44 -0700

    Moved include Pundit to the top of the class in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  protect_from_forgery with: :exception
+
 
   private
 
